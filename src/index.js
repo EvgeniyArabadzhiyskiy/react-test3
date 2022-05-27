baddf96bf31ff1624ddec959e6a741cecd450f06
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-05-27 17:33:17 +0300

    Add files

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "modern-normalize/modern-normalize.css";
+import './index.css';
+import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(
+  
+    <App />
+  
+);
+
+
+
+// npx create-react-app .    //------------- install React App -------------------
+
+
+//  npm install modern-normalize
+//  npm i prop-types
+//  npm install classnames
+//  npm install sass
+//  npm i shortid
+//  npm i axios
+
+//  npm install --save react-toastify
+//  npm install react-icons --save
+
+
+//  npm install prop-types modern-normalize classnames sass shortid axios
