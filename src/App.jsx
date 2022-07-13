d8c68fb6b487a89b8187ae3bc7722d0fa519e934
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-13 21:33:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@
 // import { ToastContainer } from "react-toastify";
 // import 'react-toastify/dist/ReactToastify.css';
 
-import Colorpicker from "./ColorPicker/ColorPicker";
-import colorPickerOptions from "./ColorPicker/colorPickerOptions.json";
+// import Colorpicker from "./ColorPicker/ColorPicker";
+// import colorPickerOptions from "./ColorPicker/colorPickerOptions.json";
 
 // import Friends from "./components/Friends/Friends";
-import Counter from "./components/Counter/Counter";
+// import Counter from "./components/Counter/Counter";
 
 // import Form from "./components/Form/Form";
 // import PokemonForm from "./components/PokemonForm/PokemonForm";
@@ -18,53 +18,107 @@ import Counter from "./components/Counter/Counter";
 
 // ======================= Маршрутизация ========================
 
+// import { lazy } from "react";
+// import { Route, Routes } from "react-router-dom";
+// import { SharedLayout } from "./components/SharedLayout/SharedLayout";
+// import { Container, Header, Logo, Link } from "./App.styled";
+
+// const createAsyncComponent = (path) => lazy(() => import(path));
+
+// import About from "./pages/About";
+// import Home from "./pages/Home";
+// import Products from "./pages/Products";
+// import NotFound from "./pages/NotFound";
+// import ProductDetails from "./pages/ProdactDetails";
+// import Statistics from "./pages/Statistics";
+// import PagePokemonInfo from "./pages/PagePokemonInfo";
+
+
+// const Home = createAsyncComponent('./pages/Home')
+// const About = createAsyncComponent('./pages/About');
+// const Products = createAsyncComponent('./pages/Products')
+// const ProductDetails = createAsyncComponent('./pages/ProdactDetails')
+// const Statistics = createAsyncComponent('./pages/Statistics')
+// const PagePokemonInfo = createAsyncComponent('./pages/PagePokemonInfo')
+// const NotFound = createAsyncComponent('./pages/NotFound')
+
+
+// const App = () => {
+//   return (
+    
+   
+//       <Routes>
+//         <Route path="/" element={<SharedLayout />}>
+
+//           <Route index element={<Home />} />
+
+//           <Route path="about" element={<About />}>
+//             {/* <Route path="color-picker" element={<Colorpicker options={colorPickerOptions} />}/> */}
+//             {/* <Route path="counter" element={<Counter />} /> */}
+//           </Route>
+//           {/* <Route path="/about/pokemons" element={<PagePokemonInfo />} /> */}
+
+//           <Route path="products" element={<Products />} />
+//           <Route path="products/:id" element={<ProductDetails />} />
+//           {/* <Route path="products/stats/:stat" element={<Statistics />} /> */}
+//           {/* <Route path="products/counter/:counter" element={<Counter />} /> */}
+
+//           <Route path="*" element={<NotFound />} />
+
+//         </Route>
+        
+//       </Routes>
+    
+//   );
+// };
+// export default App;
+
+
+// ===============================Маршрутизация разделение кода
+
+import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import { About } from "./pages/About";
-import { Home } from "./pages/Home";
-import { Products } from "./pages/Products";
-import { NotFound } from "./pages/NotFound";
-import { Container, Header, Logo, Link } from "./App.styled";
-import { ProductDetails } from "./pages/ProdactDetails";
+import { SharedLayout } from "./components/SharedLayout/SharedLayout";
 
-import Statistics from "./pages/Statistics";
-import PagePokemonInfo from "./pages/PagePokemonInfo";
+const createAsyncComponent = (path) => lazy(() => import(path));
 
-const App = () => {
+// const About = createAsyncComponent("./pages/About.jsx");
+// const Home = createAsyncComponent("./pages/Home.jsx");
+// const ProductDetails = createAsyncComponent("./pages/ProdactDetails.jsx");
+const Products = createAsyncComponent("./pages/Products");
+const NotFound = createAsyncComponent("./pages/NotFound")
+
+// const Mission = createAsyncComponent("./components/Mission.jsx");
+// const Team = createAsyncComponent("./components/Team.jsx");
+// const Reviews = createAsyncComponent("./components/Reviews.jsx");
+
+export const App = () => {
   return (
-    <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>{" "}
-          GoMerch Store
-        </Logo>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
-        </nav>
-      </Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/about" element={<About />}>
-          <Route path="color-picker" element={<Colorpicker options={colorPickerOptions} />} />
-          <Route path="counter" element={<Counter />} />
-        </Route>
-        <Route path="/about/pokemons" element={<PagePokemonInfo />} />
-
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/products/stats/:stat" element={<Statistics />} />
-        <Route path="/products/counter/:counter" element={<Counter />} />
-
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        {/* <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route> */}
+        <Route path="products" element={<Products />} />
+        {/* <Route path="products/:id" element={<ProductDetails />} /> */}
         <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Container>
+      </Route>
+    </Routes>
   );
 };
-export default App;
+
+
+
+
+
+
+
+
+
+
 
 
 
